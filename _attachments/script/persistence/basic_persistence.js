65e674578b92af63b88ebdae37ee69da5445e0e1
Ubuntu Server Account <lyubimov.denis@gmail.com>
2012-08-16 16:52:27 +0000

    Cleaning and formatting basic_persistence

--- _attachments/script/persistence/basic_persistence.js
+++ _attachments/script/persistence/basic_persistence.js
@@ -1,14 +1,9 @@
-
 // Creating graph database which would hold json that describes current
-
-
-
-
 
 function restore_link(linkObj,linkIndex){
 
-// There is a problem with link serialization as target and source are nodes objects, and inter array pointers are not preserved this function should alleviate that problem
-// TODO check if it works with node removed. It may not as index in array could be different from index property.
+    // There is a problem with link serialization as target and source are nodes objects, and inter array pointers are not preserved this function should alleviate that problem
+    // TODO check if it works with node removed. It may not as index in array could be different from index property.
     
     return {
 	source:nodes[linkObj.source.index],
@@ -16,18 +11,19 @@ function restore_link(linkObj,linkIndex){
 	};
     
 
-    }
+}
 
 
 
-ID_OF_GRAPH_STATE="1";
+ID_OF_GRAPH_STATE="1"; // id that is used for demo global graph. In future we can use it to distinct graphs
 
 restore_graph_state=function(){
-var db_restore =  new CouchDB('graph_state',{"X-Couch-Full-Commit":"false"});
+    
+    var db_restore =  new CouchDB('graph_state',{"X-Couch-Full-Commit":"false"});
 
-var graph_restore=db_restore.open(ID_OF_GRAPH_STATE);
+    var graph_restore=db_restore.open(ID_OF_GRAPH_STATE);
 
-return graph_restore;
+    return graph_restore;
 
     };
 
@@ -40,40 +36,16 @@ function restore_links(previous_graph_state){
 
 save_graph_state=function(){
 
-var db =  new CouchDB('graph_state',{"X-Couch-Full-Commit":"false"});
+    var db =  new CouchDB('graph_state',{"X-Couch-Full-Commit":"false"});
 
+    var graph_state=restore_graph_state();
 
-var graph_state=restore_graph_state();
+    graph_state.nodes=nodes;
+    graph_state.links=links;
 
-graph_state.nodes=nodes;
-
-
-
-graph_state.links=links;
-
-
-db.save(graph_state);
-
+    db.save(graph_state);
 };
 
 // We saving graph state every 5 seconds.
 
 setInterval(save_graph_state,5000);
-
-// Getting data from d3
-
-//var links_objects = vis.selectAll('line.link').data();
-
-//var nodes_objects= vis.selectAll('circle.node').data();
-
-// Reformating links data as array of {source:some_int,target:other_int}
-
-//extract_source_target=function (value,index) {
-//  return {'source': value.source.index,  
-//	    'target':value.target.index
-//	   };
-//};
-
-//var links_json_proto = $.map(links_objects,extract_source_target);
-
-// Reformatting nodes data as array of {'weight'}
