1e8438d5e602f2cf9d500600e4dabbf4cc3d69b2
Denis <lyubimov.denis@gmail.com>
2012-09-01 22:18:49 +0400

    Buggy side node editor

---
+++ _attachments/script/burp.js
@@ -0,0 +1,81 @@
+function getBurpController(input){
+
+    return {
+	
+	
+	burp_data:[],
+
+	input_object:input,
+
+
+	node_edit_end_handle: function(d){
+    
+	    var txt = this.end_edit();                                   
+	    
+	    console.log(txt);
+	    d.editorActive=false;
+	    
+	    if (txt){      
+	
+		d.nodehtml = txt;
+	    }
+	    
+	    d.showHtml = true;
+	},
+
+
+	start_edit:function(original_data){
+
+	    
+	    
+	    
+	    this.burp_data=[{original_data:original_data}];
+	    
+	    console.log(this.burp_data);
+
+	    var input_object_selection= d3.select(input)
+		.data(this.burp_data)
+		.on("blur", function(d) {
+		    
+			BurpController.node_edit_end_handle(d.original_data);
+			
+		    })
+		.on("keypress", function(d) { 
+	       
+			var e = d3.event;
+			if (e.keyCode == 13)
+			{
+			    
+			    if (e.stopPropagation)
+				e.stopPropagation();
+			    e.preventDefault();
+			
+			    console.log("enter",d);
+			    BurpController.node_edit_end_handle(d.original_data);
+			    
+			    
+			}
+
+			
+		    });
+
+
+	    this.input_object.value=original_data.nodehtml;
+
+
+	},
+	
+	end_edit:function(){
+
+//	    console.log("inner text",this.input_object.innerText);
+
+	    to_return=this.input_object.value;
+	    this.input_object.value="";
+	    return to_return;
+	
+	}
+	
+    };
+
+    
+};
