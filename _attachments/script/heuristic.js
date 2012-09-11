d64aac9299f5b1d4af4fa9a95f3c920d29b8579b
Denis <lyubimov.denis@gmail.com>
2012-09-12 03:56:48 +0400

    Changed regexp so it recognizes links without http

--- _attachments/script/heuristic.js
+++ _attachments/script/heuristic.js
@@ -1,6 +1,6 @@
 function testIfYTLink(txt){
     
-    regex = /http\:\/\/www\.youtube\.com\/watch\?v=(.{11})/;
+    regex = /www\.youtube\.com\/watch\?v=(.{11})/;
     
     return txt.match(regex);
     }
