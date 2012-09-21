499ff9fb80f5fa5eb9b151fd843a31c0afdd6611
infoburp <gwolfendale@gmail.com>
2012-09-21 11:06:21 +0100

    LABjs scriptloader

---
+++ _attachments/script/exokernel.js
@@ -0,0 +1,42 @@
+// HOWTO: load LABjs itself dynamically!
+// inline this code in your page to load LABjs itself dynamically, if you're so inclined.
+
+(function (global, oDOC, handler) {
+    var head = oDOC.head || oDOC.getElementsByTagName("head");
+
+    function LABjsLoaded() {
+        // do cool stuff with $LAB here
+    }
+
+    // loading code borrowed directly from LABjs itself
+    setTimeout(function () {
+        if ("item" in head) { // check if ref is still a live node list
+            if (!head[0]) { // append_to node not yet ready
+                setTimeout(arguments.callee, 25);
+                return;
+            }
+            head = head[0]; // reassign from live node list ref to pure node ref -- avoids nasty IE bug where changes to DOM invalidate live node lists
+        }
+        var scriptElem = oDOC.createElement("script"),
+            scriptdone = false;
+        scriptElem.onload = scriptElem.onreadystatechange = function () {
+            if ((scriptElem.readyState && scriptElem.readyState !== "complete" && scriptElem.readyState !== "loaded") || scriptdone) {
+                return false;
+            }
+            scriptElem.onload = scriptElem.onreadystatechange = null;
+            scriptdone = true;
+            LABjsLoaded();
+        };
+        scriptElem.src = "/script/LAB.js";
+        head.insertBefore(scriptElem, head.firstChild);
+    }, 0);
+
+    // required: shim for FF <= 3.5 not having document.readyState
+    if (oDOC.readyState == null && oDOC.addEventListener) {
+        oDOC.readyState = "loading";
+        oDOC.addEventListener("DOMContentLoaded", handler = function () {
+            oDOC.removeEventListener("DOMContentLoaded", handler, false);
+            oDOC.readyState = "complete";
+        }, false);
+    }
+})(window, document);
