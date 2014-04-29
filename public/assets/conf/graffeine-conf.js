64bd78a2bb7f283e3080401cf482708ef212407f
infoburp <gwolfendale@gmail.com>
2014-04-29 15:26:29 +0100

    cleaned up interface

--- public/assets/conf/graffeine-conf.js
+++ public/assets/conf/graffeine-conf.js
@@ -26,5 +26,5 @@ Graffeine.conf.graphSettings = {
 };
 
 Graffeine.conf.core = {
-    host: 'http://bender'    // where the node.js server be at
+    host: 'http://ec2-50-16-141-128.compute-1.amazonaws.com'    // where the node.js server be at
 };
